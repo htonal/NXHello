7ae2a9ad8ef09823428166a06ba80731f48e31ea
Hugues TONAL <info@tonal.fr>
2019-05-08 22:03:55 +0200

    Settings cleansing

--- NXHello/NXSettingsConfig.py
+++ NXHello/NXSettingsConfig.py
@@ -21,19 +21,4 @@ import logging.config
 
 logging.config.fileConfig('NXLogging.conf')
 logger = logging.getLogger(__name__)
-logger.debug(os.getcwd())
-
-# logger.setLevel(logging.DEBUG)
-# ch = logging.StreamHandler()
-# ch.setLevel(logging.DEBUG)
-# formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-# ch.setFormatter(formatter)
-# logger.addHandler(ch)
-
-# logger.debug('debug message')
-# logger.info('info message')
-# logger.warning('warn message')
-# logger.error('error message')
-# logger.critical('critical message')
-
 
